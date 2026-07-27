9f4f2bda6cba205c75e0303479844dcdc11fd7cf
Jae Eun <pdw05498@naver.com>
2026-07-27 23:16:20 +0900

    메세지 타입 및 장비 정보 정의
    서버와 장비간 통신 타입 및 장비가 가지고 있는 정보의 종류를 정의

---
+++ Monitoring.Shared/Models/MessageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitoring.Shared.Models;
+
+//메세지 타입을 정의
+public enum MessageType
+{
+    Register, //처음 접속할 때 장비 정보를 서버에 넘기는 용도
+    PingRequest,
+    PingResponse,
+    SystemInfoRequest,
+    SystemInfoResponse
+}
---
+++ Monitoring.Shared/Models/NetworkMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitoring.Shared.Models;
+
+//각 장비(클라이언트)에서 서버로 전송되는 메시지의 구조를 정의하는 클래스
+public class NetworkMessage
+{
+    public MessageType Type { get; set; }
+
+    public string DeviceId { get; set; } = string.Empty;
+
+    public string DeviceName { get; set; } = string.Empty;
+
+    public string RequestId { get; set; } = string.Empty;
+
+    public DateTime SentAt { get; set; } = DateTime.Now;
+
+    public string Status { get; set; } = string.Empty;
+
+    public double Temperature { get; set; }
+
+    public double Voltage { get; set; }
+
+    public int Battery { get; set; }
+}
