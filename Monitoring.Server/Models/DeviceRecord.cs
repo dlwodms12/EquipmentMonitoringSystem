20cf7f31556ad634911804dcddea3706adb90165
Jae Eun <pdw05498@naver.com>
2026-07-29 06:28:08 +0900

    DB 설치 및 DB 테이블 표현용 클래스 추가
    -SQLite 설치
    -DB 테이블 한 행을 C# 객체로 표현하기 위해 장비의 상태 정보를 담고 있는 DeviceRecord 클래스 추가

---
+++ Monitoring.Server/Models/DeviceRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace Monitoring.Server.Models;
+// DB의 Devices 테이블 한 행을 C# 객체로 표현하는 클래스, 장비의 상태 정보를 담고 있음
+public class DeviceRecord
+{
+    public string DeviceId { get; set; } = string.Empty;
+
+    public string DeviceName { get; set; } = string.Empty;
+
+    public string Status { get; set; } = "정보 없음";
+
+    public double Temperature { get; set; }
+
+    public double Voltage { get; set; }
+
+    public int Battery { get; set; }
+
+    public bool IsConnected { get; set; }
+
+    public DateTime? LastSeen { get; set; }
+}
